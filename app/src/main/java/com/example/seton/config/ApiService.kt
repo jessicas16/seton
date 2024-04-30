5069d5bad10c19e0878575319e6404ded4eb72f0
jees <101440484+jessicas16@users.noreply.github.com>
2024-04-30 10:11:57 +0700

    still error

--- app/src/main/java/com/example/seton/config/ApiService.kt
+++ app/src/main/java/com/example/seton/config/ApiService.kt
@@ -1,4 +1,18 @@
 package com.example.seton.config
 
+import com.example.seton.entity.userEntity
+import retrofit2.http.*
+
 interface apiService {
+    //USER
+    @GET("users")
+    suspend fun getAllUser(@Query("q") q:String = ""):List<userEntity>
+
+    @POST("users/register")
+    suspend fun registerUser(
+        @Body email:String, name:String, password:String
+    ):String
+
+    @POST("users/login")
+    suspend fun loginUser(@Body user:userEntity):userEntity
 }
