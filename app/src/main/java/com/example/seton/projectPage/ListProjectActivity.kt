c67da2f9409e85dfb5a225c812bffa59478da39e
Ivan Susanto <105846193+ivansusanto@users.noreply.github.com>
2024-05-04 01:24:43 +0700

    init

--- app/src/main/java/com/example/seton/projectPage/ListProjectActivity.kt
+++ app/src/main/java/com/example/seton/projectPage/ListProjectActivity.kt
@@ -3,34 +3,42 @@ package com.example.seton.projectPage
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.constraintlayout.compose.ConstraintLayout
 
 class ListProjectActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            GreetingPreview()
+            ListProjectPreview()
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ListProjectPreview() {
+    ConstraintLayout(modifier = Modifier.fillMaxSize()) {
+        LazyColumn(Modifier.fillMaxSize()) {
+            val data = listOf<String>("Budi", "Amir", "Yanto")
+            items(data) {
+                ListItem(it)
+            }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    val a = "asda"
-    Greeting(name = a, Modifier.fillMaxSize())
+fun ListItem(data: String, modifier: Modifier = Modifier) {
+    Row(modifier.fillMaxWidth()) {
+        Text(text = data)
+    }
 }
