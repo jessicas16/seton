c416a07cdb33f5d0708ccdbf8f9afa5af906a28a
Ivan Susanto <105846193+ivansusanto@users.noreply.github.com>
2024-05-05 17:53:22 +0700

    benerin ignore env

--- app/src/main/java/com/example/seton/env.kt
+++
@@ -1,8 +0,0 @@
-package com.example.seton
-
-object env {
-    const val mode: String = "local" //production or local
-//    const val URL: String = "http://<IP Address>:3000/api/"
-//    const val URL: String = "http://192.168.100.58:3000/api/" //vansus
-    const val URL: String = "http://192.168.191.60:3000/api/" //jees
-}
